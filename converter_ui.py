bc983a41eb24f49c54dcdf71a71fdcfb2e4cfb28
dtg01100 <dlafreniere349@gmail.com>
2015-06-02 12:34:56 -0400

    Revert "disable window resize"
    This reverts commit 5234a80ec829ffa99d307d885951dbda38110f63.

--- converter_ui.py
+++ converter_ui.py
@@ -10,8 +10,7 @@ filename = '' #initialize filename Variable
 root = Tk() # initialize UI Window component
 root.title("EDI to CSV converter") # Set Window Title
 root.geometry("650x170") # Set initial window size
-#root.minsize(width=650, height=170) # Set window minimal Size
-root.resizable(0,0) # disable window resize
+root.minsize(width=650, height=170) # Set window minimal Size
 var = StringVar() # Status bar Variable
 var.set('Select File') # Set initial Status bar status
 frame = Frame(root) # Set placement frame
