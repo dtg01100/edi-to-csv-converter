bdc983215d0bf76674fcb1920068ad315b961764
dtg01100 <dlafreniere349@gmail.com>
2015-06-05 13:55:36 -0400

    moved all column creation into same loop

--- converter.py
+++ converter.py
@@ -20,6 +20,7 @@ def edi_convert(edi_process, output_filename, calc_upc, inc_arec, inc_crec, inc_
     work_file_count = open(edi_process)
     cut_off = sum(1 for line in work_file_count)
     work_file = [n for n in work_file.readlines() if not n.startswith('A') if not n.startswith('C')]
+
     for line_num, line in enumerate(work_file):
         if line_num < cut_off:
             #print line[1:12]
@@ -27,71 +28,16 @@ def edi_convert(edi_process, output_filename, calc_upc, inc_arec, inc_crec, inc_
                 column1.append(line[1:12])
             else:
                 column1.append(upc_check_digit.add_check_digit(line[1:12]))
-    #print (column1)
-
-    work_file = open(edi_process)
-    work_file_count = open(edi_process)
-    cut_off = sum(1 for line in work_file_count)
-    work_file = [n for n in work_file.readlines() if not n.startswith('A') if not n.startswith('C')]
-    for line_num, line in enumerate(work_file):
-        if line_num < cut_off:
-            #print line[12:37]
             if filter_ampersand != 0:
                 linetemp = line[12:37]
                 column2.append(linetemp.replace("&","AND"))
             else:
                 column2.append(line[12:37])
-    #print (column2)
-
-    work_file = open(edi_process)
-    work_file_count = open(edi_process)
-    cut_off = sum(1 for line in work_file_count)
-    work_file = [n for n in work_file.readlines() if not n.startswith('A') if not n.startswith('C')]
-    for line_num, line in enumerate(work_file):
-        if line_num < cut_off:
-            #print line[38:43]
             column3.append(line[38:43])
-    #print (column3)
-
-    work_file = open(edi_process)
-    work_file_count = open(edi_process)
-    cut_off = sum(1 for line in work_file_count)
-    work_file = [n for n in work_file.readlines() if not n.startswith('A') if not n.startswith('C')]
-    for line_num, line in enumerate(work_file):
-        if line_num < cut_off:
-            #print line[45:49]
             column4.append(line[45:47] + "." + line[47:49])
-    #print (column4)
-
-    work_file = open(edi_process)
-    work_file_count = open(edi_process)
-    cut_off = sum(1 for line in work_file_count)
-    work_file = [n for n in work_file.readlines() if not n.startswith('A') if not n.startswith('C')]
-    for line_num, line in enumerate(work_file):
-        if line_num < cut_off:
-            #print line[55:57]
             column5.append(line[55:57])
-    #print (column5)
-
-    work_file = open(edi_process)
-    work_file_count = open(edi_process)
-    cut_off = sum(1 for line in work_file_count)
-    work_file = [n for n in work_file.readlines() if not n.startswith('A') if not n.startswith('C')]
-    for line_num, line in enumerate(work_file):
-        if line_num < cut_off:
-            #print line[63:67]
             column6.append(line[63:65] + "." + line[65:67])
-    #print (column6)
-
-    work_file = open(edi_process)
-    work_file_count = open(edi_process)
-    cut_off = sum(1 for line in work_file_count)
-    work_file = [n for n in work_file.readlines() if not n.startswith('A') if not n.startswith('C')]
-    for line_num, line in enumerate(work_file):
-        if line_num < cut_off:
-            #print line[60:62]
             column7.append(line[60:62])
-    #print (column7)
 
     if conv_inc_arec != 0:
         f = open(output_filename, "w")
