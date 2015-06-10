732a102c346704d8c2a2d8fb2c27f2b21b6be269
dtg01100 <dlafreniere349@gmail.com>
2015-06-10 10:15:35 -0400

    initial implementation

--- converter.py
+++ converter.py
@@ -19,75 +19,35 @@ def edi_convert(edi_process, output_filename, calc_upc, inc_arec, inc_crec, inc_
     work_file = open(edi_process)
     work_file_count = open(edi_process)
     cut_off = sum(1 for line in work_file_count)
-    work_file = [n for n in work_file.readlines() if n.startswith('B')]
+    work_file_lined = [n for n in work_file.readlines()]
 
-    for line_num, line in enumerate(work_file):
-        if line_num < cut_off:
-            # print line[1:12]
-            if conv_calc_upc != 1:
-                column1.append(line[1:12])
-            else:
-                column1.append(upc_check_digit.add_check_digit(line[1:12]))
-            if filter_ampersand != 0:
-                linetemp = line[12:37]
-                column2.append(linetemp.replace("&", "AND"))
-            else:
-                column2.append(line[12:37])
-            column3.append(line[38:43])
-            column4.append(line[45:47] + "." + line[47:49])
-            column5.append(line[55:57])
-            column6.append(line[63:65] + "." + line[65:67])
-            column7.append(line[60:62])
-
-    if conv_inc_arec != 0:
-        f = open(output_filename, "w")
-        work_file = open(edi_process)
-        work_file_count = open(edi_process)
-        cut_off = sum(1 for line in work_file_count)
-        work_file = [n for n in work_file.readlines() if n.startswith('A')]
-        for line_num, line in enumerate(work_file):
-            if line_num < cut_off:
-                # print line[:]
-                column_prepend.append(line[:])
-        # print (column_prepend)
-        f.seek(0, 0)
-        for i in xrange(len(column_prepend)):
-            f.write("{}".format(column_prepend[i]))
-        f.close()
 
     if conv_inc_headers != 0:
-        if conv_inc_arec != 0:
-            f = open(output_filename, "a")
-        else:
-            f = open(output_filename, "w")
+        f = open(output_filename, "w")
 
         f.write("{}" "," "{}" "," "{}" "," "{}" "," "{}" "," "{}" "," "{}\n".format("UPC", "Qty. Shipped", "Cost",
                 "Suggested Retail", "Description", "Case Pack", "Item Number"))
         f.close()
 
-    if conv_inc_arec != 0 or conv_inc_headers != 0:
-        f = open(output_filename, "a")
-    else:
-        f = open(output_filename, "w")
-
-    for i in xrange(len(column1)):
-        f.write("{}\t" "," "{}\t" "," "{}\t" "," "{}\t" "," "{}" "," "{}\t" "," "{}\t" ",\n".format(column1[i],
-                column7[i], column4[i], column6[i], column2[i], column5[i], column3[i]))
-    f.close()
-
-    if conv_inc_crec != 0:
-        f = open(output_filename, "a")
-
-        work_file = open(edi_process)
-        work_file_count = open(edi_process)
-        cut_off = sum(1 for line in work_file_count)
-        work_file = [n for n in work_file.readlines() if n.startswith('C')]
-        for line_num, line in enumerate(work_file):
+    f = open(output_filename, "a")
+    for line_num, line in enumerate(work_file_lined):
+        if n.startswith('A'):
+            f.write("{}".format(line[:]))
+        if n.startswith('B'):
             if line_num < cut_off:
-                # print line[1:14] + " " + line[33:35] + "." + line[35:-2]
-                column_append.append(line[:1] + " " + line[1:14] + " " + line[-5:-3] + "." + line[-3:-1])
-        # print (column_append)
-        f.seek(0, 0)
-        for i in xrange(len(column_append)):
-            f.write("{}".format(column_append[i]))
-        f.close()
+                # print line[1:12]
+                if conv_calc_upc != 1 and filter_ampersand != 1:
+                    f.write("{}\t" "," "{}\t" "," "{}\t" "," "{}\t" "," "{}" "," "{}\t" "," "{}\t" ",\n".format(line[1:12],
+        line[60:62], line[45:47] + "." + line[47:49], line[63:65] + "." + line[65:67], line[12:37], line[55:57], line[38:43]))
+                elif conv_calc_upc != 0 and filter_ampersand != 1:
+                    f.write("{}\t" "," "{}\t" "," "{}\t" "," "{}\t" "," "{}" "," "{}\t" "," "{}\t" ",\n".format(upc_check_digit.add_check_digit(line[1:12]),
+        line[60:62], line[45:47] + "." + line[47:49], line[63:65] + "." + line[65:67], line[12:37], line[55:57], line[38:43]))
+                elif conv_calc_upc !=0 and filter_ampersand != 0:
+                    f.write("{}\t" "," "{}\t" "," "{}\t" "," "{}\t" "," "{}" "," "{}\t" "," "{}\t" ",\n".format(upc_check_digit.add_check_digit(line[1:12]),
+        line[60:62], line[45:47] + "." + line[47:49], line[63:65] + "." + line[65:67], line.replace("&", "AND")[12:37], line[55:57], line[38:43]))
+                elif conv_calc_upc !=1 and filter_ampersand !=0:
+                    f.write("{}\t" "," "{}\t" "," "{}\t" "," "{}\t" "," "{}" "," "{}\t" "," "{}\t" ",\n".format(line[1:12],
+        line[60:62], line[45:47] + "." + line[47:49], line[63:65] + "." + line[65:67], line.replace("&", "AND")[12:37], line[55:57], line[38:43]))
+        if n.startswith('C'):
+            f.write("{}".format(line[:]))
+    f.close()
